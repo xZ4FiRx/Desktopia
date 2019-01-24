728805aa832740d69458b7d821aa9c0f0cf1eac4
Zafir <zafir.mcbryde@gtl.net>
2019-01-24 07:58:50 -0500

    Mostly UI updates.

---
+++ app/src/main/java/com/example/z4fir/desktopia/Fragments/ViewPagerFragments/InstagramFeedFragment.kt
@@ -0,0 +1,31 @@
+package com.example.z4fir.desktopia.Fragments.ViewPagerFragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.z4fir.desktopia.Fragments.GridFragment
+
+import com.example.z4fir.desktopia.R
+import org.jetbrains.anko.support.v4.toast
+
+
+class InstagramFeedFragment : Fragment()
+{
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    {
+        var view: View = inflater.inflate(R.layout.fragment_instagram_feed, container, false)
+
+
+
+        return view
+    }
+
+    companion object
+    {
+        fun newInstance(): InstagramFeedFragment = InstagramFeedFragment()
+    }
+}
--- app/src/main/java/com/example/z4fir/desktopia/MainActivity.kt
+++ app/src/main/java/com/example/z4fir/desktopia/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.z4fir.desktopia
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import android.support.annotation.NonNull
 import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.TabLayout
@@ -11,20 +12,18 @@ import android.view.MenuItem
 import com.example.z4fir.desktopia.Fragments.GridFragment
 import com.example.z4fir.desktopia.Fragments.HelpFragment
 import com.example.z4fir.desktopia.Fragments.PurchaseFragment
+import com.lockwood.memorizingpager.NavigationHistory
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.toolbar
 
 class MainActivity : AppCompatActivity()
 {
 
-    lateinit var toolbar: ActionBar
-
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        toolbar = supportActionBar!!
         val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_nav)
         val firstFrag = GridFragment.newInstance()
 
@@ -34,6 +33,7 @@ class MainActivity : AppCompatActivity()
 
     }
 
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId)
         {
@@ -41,21 +41,18 @@ class MainActivity : AppCompatActivity()
             {
                 val firstFrag = GridFragment.newInstance()
                 openFragment(firstFrag)
-                toast("First")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_second ->
             {
                 val secondFrag = PurchaseFragment.newInstance()
                 openFragment(secondFrag)
-                toast("Second")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_last ->
             {
                 val lastFrag = HelpFragment.newInstance()
                 openFragment(lastFrag)
-                toast("Last")
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -66,7 +63,6 @@ class MainActivity : AppCompatActivity()
     {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.container, fragment)
-        transaction.addToBackStack(null)
         transaction.commit()
     }
 }
