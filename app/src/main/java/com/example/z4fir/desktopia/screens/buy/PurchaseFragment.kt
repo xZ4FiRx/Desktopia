3c61ab0c38ff5f4894db188699d039c9c205fc76
Zafir <zafir.mcbryde@gtl.net>
2019-12-05 09:27:51 -0500

    fixed and improved some stuff...?

--- app/src/main/java/com/example/z4fir/desktopia/screens/buy/PurchaseFragment.kt
+++ app/src/main/java/com/example/z4fir/desktopia/screens/buy/PurchaseFragment.kt
@@ -15,7 +15,6 @@ class PurchaseFragment : Fragment()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
-
         return inflater.inflate(R.layout.fragment_purchase, container, false)
     }
 }
